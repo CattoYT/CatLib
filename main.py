c957ede12301a63443546c5680faa5e8d4d58c4b
CattoYT <97292582+CattoYT@users.noreply.github.com>
2023-04-26 13:10:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,110 @@
+import turtle as t
+import random as r
+
+def drawSquare(size):
+    
+    for i in range(4):
+        t.forward(size)
+        t.right(90)
+
+
+def drawRect(width, height, color):
+    t.begin_fill()
+    if color == None:
+        t.color("black")
+    else:
+        t.color(color)
+    t.setheading(90)
+    for i in range(2):
+        t.forward(height)
+        t.right(90)
+        t.forward(width)
+        t.right(90)
+    t.end_fill()
+
+def drawTriangle(size):
+    for i in range(3):
+        t.forward(size)
+        t.right(120)
+
+def drawCircle(size):
+    t.circle(size)
+
+def drawCustom(sides, size, angle, color):
+    t.color(color)
+    for i in range(sides):
+        t.forward(size)
+        t.right(angle)
+
+def drawRandom(size):
+
+    temp = 60
+    print(temp)
+    for i in range(temp):
+        t.forward(size)
+        t.right(((temp-2)*360)/temp)
+
+
+#29/3/23 additions
+
+def draw5PointStar(color):
+    t.color(color)
+    t.begin_fill()
+    for i in range(5):
+
+       t.forward(100)
+
+       t.right(144)
+    t.end_fill()
+def draw2LayeredFlag(color1, color2):
+    drawRect(50, 200, color1)
+    t.setheading(270)
+    t.forward(50)
+    drawRect(50, 200, color2)
+
+def draw3LayeredVerticalFlag(color1, color2, color3, borderStatus):
+    drawRect(50, 150, color1)
+    t.setheading(0)
+    t.forward(50)
+    t.left(90)
+    drawRect(50, 150, color2)
+    t.setheading(0)
+    t.forward(50)
+    t.left(90)
+    drawRect(50, 150, color3)
+    if borderStatus:
+        t.setheading(0)
+        t.forward(50)
+        t.setheading(180)
+        t.color("black")
+        for i in range(2):
+            t.forward(150)
+            t.right(90)
+            t.forward(150)
+            t.right(90)
+
+
+
+def drawFlag(country):
+    
+    if country == "somalia":
+        drawRect(300, 150, "cyan")
+
+
+        t.setheading(90)
+        t.forward(100)
+        t.setheading(0)
+        t.forward(100)
+        draw5PointStar("white")
+    if country == "vietnam":
+        drawRect(300, 150, "red")
+
+
+        t.setheading(90)
+        t.forward(100)
+        t.setheading(0)
+        t.forward(100)
+        draw5PointStar("yellow")
+
+
+    
