53424a5813c785331721bce11030ea34acdedfc7
CattoYT <97292582+CattoYT@users.noreply.github.com>
2023-05-10 13:00:05 +0100

    now object oriented

--- main.py
+++ main.py
@@ -1,118 +1,103 @@
 import random as r
 import turtle as t
 
-def drawSquare(size):
-    
-    for i in range(4):
-        t.forward(size)
-        t.right(90)
+class catLib:
+    def __init__(self, turtle):
+        self.t = turtle
 
+    def drawSquare(self, size):
+        for _ in range(4):
+            self.t.forward(size)
+            self.t.right(90)
 
-def drawRect(width, height, color):
-    t.begin_fill()
-    if color == None:
-        t.color("black")
-    else:
-        t.color(color)
-    t.setheading(90)
-    for i in range(2):
-        t.forward(height)
-        t.right(90)
-        t.forward(width)
-        t.right(90)
-    t.end_fill()
+    def drawRect(self, width, height, color=None):
+        self.t.begin_fill()
+        if color is None:
+            self.t.color("black")
+        else:
+            self.t.color(color)
+        self.t.setheading(90)
+        for _ in range(2):
+            self.t.forward(height)
+            self.t.right(90)
+            self.t.forward(width)
+            self.t.right(90)
+        self.t.end_fill()
 
-def drawTriangle(size, filled, color):
-    if filled:
-        previousColor = t.pencolor()
-        t.color(color)
-        t.begin_fill()
-    for i in range(3):
-        t.forward(size)
-        t.right(120)
-    if filled:
-        t.end_fill()
-        t.color(previousColor)
-        del(previousColor)
+    def drawTriangle(self, size, filled, color):
+        if filled:
+            previousColor = self.t.pencolor()
+            self.t.color(color)
+            self.t.begin_fill()
+        for _ in range(3):
+            self.t.forward(size)
+            self.t.right(120)
+        if filled:
+            self.t.end_fill()
+            self.t.color(previousColor)
+            del(previousColor)
 
-def drawCircle(size):
-    t.circle(size)
+    def drawCircle(self, size):
+        self.t.circle(size)
 
-def drawCustom(sides, size, angle, color):
-    t.color(color)
-    for i in range(sides):
-        t.forward(size)
-        t.right(angle)
+    def drawCustom(self, sides, size, angle, color):
+        self.t.color(color)
+        for _ in range(sides):
+            self.t.forward(size)
+            self.t.right(angle)
 
-def drawRandom(size):
+    def drawRandom(self, size):
+        temp = 60
+        print(temp)
+        for _ in range(temp):
+            self.t.forward(size)
+            self.t.right(((temp - 2) * 360) / temp)
 
-    temp = 60
-    print(temp)
-    for i in range(temp):
-        t.forward(size)
-        t.right(((temp-2)*360)/temp)
+    def draw5PointStar(self, color):
+        self.t.color(color)
+        self.t.begin_fill()
+        for _ in range(5):
+            self.t.forward(100)
+            self.t.right(144)
+        self.t.end_fill()
 
+    def draw2LayeredFlag(self, color1, color2):
+        self.drawRect(50, 200, color1)
+        self.t.setheading(270)
+        self.t.forward(50)
+        self.drawRect(50, 200, color2)
 
-#29/3/23 additions
+    def draw3LayeredVerticalFlag(self, color1, color2, color3, borderStatus):
+        self.drawRect(50, 150, color1)
+        self.t.setheading(0)
+        self.t.forward(50)
+        self.t.left(90)
+        self.drawRect(50, 150, color2)
+        self.t.setheading(0)
+        self.t.forward(50)
+        self.t.left(90)
+        self.drawRect(50, 150, color3)
+        if borderStatus:
+            self.t.setheading(0)
+            self.t.forward(50)
+            self.t.setheading(180)
+            self.t.color("black")
+            for _ in range(2):
+                self.t.forward(150)
+                self.t.right(90)
 
-def draw5PointStar(color):
-    t.color(color)
-    t.begin_fill()
-    for i in range(5):
-
-       t.forward(100)
-
-       t.right(144)
-    t.end_fill()
-def draw2LayeredFlag(color1, color2):
-    drawRect(50, 200, color1)
-    t.setheading(270)
-    t.forward(50)
-    drawRect(50, 200, color2)
-
-def draw3LayeredVerticalFlag(color1, color2, color3, borderStatus):
-    drawRect(50, 150, color1)
-    t.setheading(0)
-    t.forward(50)
-    t.left(90)
-    drawRect(50, 150, color2)
-    t.setheading(0)
-    t.forward(50)
-    t.left(90)
-    drawRect(50, 150, color3)
-    if borderStatus:
-        t.setheading(0)
-        t.forward(50)
-        t.setheading(180)
-        t.color("black")
-        for i in range(2):
-            t.forward(150)
-            t.right(90)
-            t.forward(150)
-            t.right(90)
-
-
-
-def drawFlag(country):
-    
-    if country == "somalia":
-        drawRect(300, 150, "cyan")
-
-
-        t.setheading(90)
-        t.forward(100)
-        t.setheading(0)
-        t.forward(100)
-        draw5PointStar("white")
-    if country == "vietnam":
-        drawRect(300, 150, "red")
-
-
-        t.setheading(90)
-        t.forward(100)
-        t.setheading(0)
-        t.forward(100)
-        draw5PointStar("yellow")
-
-
-    
+    def drawFlag(self, country):
+        if country == "somalia":
+            self.drawRect(300, 150, "cyan")
+            self.t.setheading(90)
+            self.t.forward(100)
+            self.t.setheading(0)
+            self.t.forward(100)
+            self.draw5PointStar("white")
+        elif country == "vietnam":
+            self.drawRect(300, 150, "red")
+            self.t.setheading(90)
+            self.t.forward(100)
+            self.t.setheading(0)
+            self.t.forward(100)
+            self.draw5PointStar("yellow")
